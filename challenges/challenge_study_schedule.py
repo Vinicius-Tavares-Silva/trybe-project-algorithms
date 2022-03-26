c87c1ddd8747174891ad9930b7247d87d7c96568
Vinicius Tavares <vini_tavares10@hotmail.com>
2022-03-26 12:28:58 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    
+    if target_time is None:
+        return None
+    count = 0
+    for period in permanence_period:
+        begin, end = period
+        if ((type(begin) or type(end)) == str) or None in period:
+            return None
+        if begin <= target_time <= end:
+            count += 1
+    return count
